db2cf65edfda2bc46d2d559be782fbd361535a15
beastrobel <beatrizstrobel@proton.me>
2023-10-15 05:15:43 -0400

    Added setColor() and render() functions

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ inquirer
     {
         type: 'checkbox',
         message: 'Pick a shape:',
-        name: 'mark',
+        name: 'shape',
         choices: ['Circle', 'Triangle', 'Square'],
     },
     {
@@ -28,7 +28,7 @@ inquirer
 ]) 
 .then((data) => {  
         //Creates JSON file that can be used for testing
-        fs.writeFile('logo-info.json', JSON.stringify(data, null, ' '), (err) =>
+        fs.writeFile('logo-info.json', JSON.stringify.toLowerCase(data, null, ' '), (err) =>
         err ? console.log(err) : console.log('Created logo-info.json'));
 });
 
@@ -37,11 +37,11 @@ inquirer
 //fs.writeFile('logo.svg', data, (err) =>
 //err ? console.log('write error') : console.log('Generated logo.svg'));
 
-render() {
-    return `<!DOCTYPE html>
-    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="150" cy="100" r="80" fill='${this.color}' />
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-    </svg>
-    </html>`;
-  }
+// render() {
+//     return `<!DOCTYPE html>
+//     <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+//     <circle cx="150" cy="100" r="80" fill='${this.color}' />
+//     <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+//     </svg>
+//     </html>`;
+//   }
