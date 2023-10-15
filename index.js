0f66972e02ebceb032ae5ff26c55b9a220d1cf8e
beastrobel <beatrizstrobel@proton.me>
2023-10-14 21:46:24 -0400

    Added prompts

--- index.js
+++ index.js
@@ -3,5 +3,28 @@ const inquirer = require('inquirer');
 const jest = require('jest');
 const fs = require('fs');
 
+//Logo maker prompts
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'What text would you like for your logo? (Max. 3 letters)',
+        name: 'text',
+    },
+    {
+        type: 'input',
+        message: 'What color would you like? (Type in color name or HEX#)',
+        name: 'color',
+    },
+    {
+        type: 'checkbox',
+        message: 'Pick a shape:',
+        name: 'mark',
+        choices: ['Circle', 'Triangle', 'Square'],
+    },
+]) 
+.then((data) => {
+    console.log('Success!');
+});
 
 
